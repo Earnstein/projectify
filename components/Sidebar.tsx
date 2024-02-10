11641a944d3e560b4de99ae8e234a3684b2d88cf
Bakare Damilola <bdamilola00@gmail.com>
2024-02-10 14:07:52 +0100

    chore: refactor style on sidebar and navbar layout

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Playfair, Palanquin } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessagesSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const playfair = Playfair({
   weight: "600",
@@ -23,7 +24,7 @@ const routes = [
     color: "text-sky-500",
   },
   {
-    label: "conversation",
+    label: "Conversation",
     icon: MessagesSquare,
     href: "/conversation",
     color: "text-violet-500",
@@ -60,9 +61,14 @@ const routes = [
   },
 ];
 const Sidebar = () => {
+  const pathname = usePathname();
+
+
   return (
     <div className="space-y-4 py-4 flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
+        
+        {/* HEADER */}
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-4">
             <Image fill alt="logo" src="/logo.png" />
@@ -72,6 +78,7 @@ const Sidebar = () => {
           </h1>
         </Link>
 
+        {/* LINKS */}
         <div className="space-y-1">
           {routes.map((route) => (
             <Link
@@ -79,7 +86,8 @@ const Sidebar = () => {
               key={route.href}
               className={cn(
                 "group text-sm flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-                palanquin.className
+                palanquin.className,
+                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
               )}
             >
               <div className="flex items-center flex-1">
@@ -89,6 +97,7 @@ const Sidebar = () => {
             </Link>
           ))}
         </div>
+
       </div>
     </div>
   );

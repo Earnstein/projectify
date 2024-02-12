447f6cda99fa2187529eb873c58d353ef27d4663
bakare damilola <109681527+Earnstein@users.noreply.github.com>
2024-02-12 01:29:41 +0100

    Update constants.ts

--- app/(dashboard)/(routes)/code/constants.ts
+++ app/(dashboard)/(routes)/code/constants.ts
@@ -9,7 +9,7 @@ export const formSchema = z.object({
 
 export const DEFAULT_PROMPT = {
     "role": "system",
-    "content": "You are Peterlight, \
+    "content": "Your name is Peterlight, \
      a code generator and instructor for Excel, MS Project and many other softwares and tools only.\
      Provide code snippets only in markdown and use comments for explanations \
      if user asks questions outside this , respond with 'I am only good at generating code or teaching you how to programs 😎 ' \

090ed812a205f186509ea3d4f1c7582bf70c74fa
Bakare Damilola <bdamilola00@gmail.com>
2024-02-12 16:42:55 +0100

    .

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -29,14 +29,19 @@ export async function POST(
       if (!messages) {
         return new NextResponse("Messages are required", { status: 400 });
       }
-      
+      const freeTrial = await checkApiLimit();
+
+      if (!freeTrial){
+        return new NextResponse("Free trial has expired.", {status: 403});
+      }
   
+      
       const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [DEFAULT_PROMPT, ...messages]
       });   
   
-      
+      await increaseApiLimit();
       return NextResponse.json(response.choices[0].message);
     } catch (error) {
       console.log('[CONVERSATION_ERROR]', error);
